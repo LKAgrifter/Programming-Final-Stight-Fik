365b8e29eaa058065e624fe3796cb30319a47a0e
Matt <h2ogrifter@gmail.com>
2022-05-04 12:31:22 -0400

    other thing

--- TheGame.py
+++ TheGame.py
@@ -13,6 +13,7 @@ BUFFRERSIZE = 2048
 serverip = 'minecraft.skilakeanna.com'
 port = 4321
 run = False
+name='Billy Bob'
 
 pygame.init()
 win = pygame.display.set_mode((1920,1080), pygame.FULLSCREEN)
@@ -28,10 +29,10 @@ def start_game():
      run = True
 #     menu.disable()
      print('billybob')
-menu = pygame_menu.Menu('Welcome', 1920, 1080,theme=pygame_menu.themes.THEME_BLUE)
-menu.add.text_input('Name: ', default='John Doe')
-serverip = menu.add.text_input('Server Adress: ', default='mc.skilakeanna.com')
-port = menu.add.text_input('Sever Port: ', default='4321')
+menu = pygame_menu.Menu('Welcome To Stight Fik', 1920, 1080,theme=pygame_menu.themes.THEME_BLUE)
+name_prompt = menu.add.text_input('Name: ', default='John Doe')
+serverip_prompt = menu.add.text_input('Server Address: ', default='mc.skilakeanna.com')
+port_prompt = menu.add.text_input('Sever Port: ', default=4321)
 menu.add.button('Play', start_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
@@ -58,11 +59,15 @@ class Player(object):
           self.pos = self.vec((x, y))
           self.vel = self.vec(0,0)
           self.acc = self.vec(0,0)
-player = Player(200,200,1)
+     def keyinput(self,keys):
+          if keys[pygame.K_LEFT]:
+               x-=xvel
+          if keys[pygame.K_RIGHT]:
+               x+=xvel
+          if keys[pygame.K_UP] and isJumping==False:
+               yvel=-1
+               isJumping=True
 
-
-
-#Player(100,100,1)        
 x = 40
 y = 30
 width = 48
@@ -73,12 +78,22 @@ isJumping=False
 
 
 while True:
- #menu handling
+ #menu handling      
+ # Name, IP, and Port
+ if name_prompt.get_value() != name:
+      name = name_prompt.get_value()
+ if serverip_prompt.get_value() != serverip:
+      serverip = serverip_prompt.get_value()
+ if port_prompt.get_value() != port:
+      port = port_prompt.get_value()
  events = pygame.event.get()
  if run == False:
      menu.update(events)
      menu.draw(win)
      pygame.display.update()
+     if port_prompt.get_value() != port:
+          port = port_prompt.get_value()
+
  #game handling
  if y+128>1080-220:
       isJumping=False
@@ -102,6 +117,11 @@ while True:
      player = Player(200,200,2)
      Player2 = Player(800,300,3)
      Player3 = Player(1600,400,4)
+ if keys[pygame.K_p]:
+     print(port)
+     print(serverip)
+     print(name)
+
  pygame.display.update()
 
 pygame.quit()
