e3e80be41f35ce15244f6356085f7026f35d416d
Matt <h2ogrifter@gmail.com>
2022-05-10 12:04:20 -0400

    import required packages

--- TheGame.py
+++ TheGame.py
@@ -1,19 +1,6 @@
-import sys
-import subprocess
-
-# implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-'pygame'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-'pygame_menu'])
-
-# process output with an API in the subprocess module:
-reqs = subprocess.check_output([sys.executable, '-m', 'pip',
-'freeze'])
-installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
-
-print(installed_packages)
-
+from gettext import install
+from importstuffs import installRequiredPackages
+installRequiredPackages()
 
 import pygame
 import sys
